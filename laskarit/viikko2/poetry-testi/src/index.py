49bb25b67bc1073a8af12faef2c214006afdfdb0
mirelle01 <lybackmirelle@gmail.com>
2022-03-25 13:47:05 +0200

    unohdin poetry-test

---
+++ laskarit/viikko2/poetry-testi/src/index.py
@@ -0,0 +1,13 @@
+import cowsay 
+
+#cowsay.tux("Poetry is awesome!")
+
+print(cowsay.get_output_string('ghostbusters', 'Hello (extinct) World'))
+#cowsay.dragon('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit rhoncus nibh. Mauris mi mauris, molestie vel metus sit amet, aliquam vulputate nibh.')
+
+cowsay.char_names
+['beavis', 'cheese', 'daemon', 'cow', 'dragon', 'ghostbusters', 'kitty', 'meow', 'milk', 'pig', 'stegosaurus', 'stimpy', 'trex', 'turkey', 'turtle', 'tux']
+
+
+len(cowsay.chars)
+16
