fb4790f7a7b79c43f2c8e15a286d94845d6a20ee
mirelle01 <lybackmirelle@gmail.com>
2022-05-03 13:18:42 +0300

    quit painike

--- src/ui/game_ui.py
+++ src/ui/game_ui.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from ui.game import MainRun
 from settings import clock, window, white, pink, start_game, \
-    font, largefont, victory, play_again, defeat
+    font, largefont, victory, play_again, defeat, quit_game
 
 class Main():
     def __init__(self):
@@ -35,13 +35,22 @@ class Main():
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if 50 <= mouse[0] <= 450 and 250 <= mouse[1] <= 350:
+                    if 50 <= mouse[0] <= 450 and 150 <= mouse[1] <= 250:
                         self.run_game()
-            pygame.draw.rect(window, white, pygame.Rect(50, 250, 400, 100))
-            if 50 <= mouse[0] <= 450 and 250 <= mouse[1] <= 350:
+                    if 50 <= mouse[0] <= 450 and 300 <= mouse[1] <= 400:
+                        pygame.quit()
+                        sys.exit()
+                    
+            pygame.draw.rect(window, white, pygame.Rect(50, 150, 400, 100))
+            if 50 <= mouse[0] <= 450 and 150 <= mouse[1] <= 250:
                 pygame.draw.rect(window, (242, 242, 242),
-                                 pygame.Rect(50, 250, 400, 100))
-            window.blit(start_game, (120, 270))
+                                 pygame.Rect(50, 150, 400, 100))
+            window.blit(start_game, (180, 170))
+            pygame.draw.rect(window, white, pygame.Rect(50, 300, 400, 100))
+            if 50 <= mouse[0] <= 450 and 300 <= mouse[1] <= 400:
+                pygame.draw.rect(window, (242, 242, 242),
+                                 pygame.Rect(50, 300, 400, 100))
+            window.blit(quit_game, (180, 330))
             pygame.display.update()
 
     def win_menu(self):
